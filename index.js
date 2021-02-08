0e73542801d12fb908c5a1cf54a28065f71e15c2
ahmedhabeeb73 <alnaib888@gmail.com>
2021-02-08 21:14:00 +0100

    env platform check

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const {app,BrowserWindow}= require('electron')
 
+
+// set env
+process.env.NODE_ENV='development'
+const isDev=process.env.NODE_ENV !=='production' ?true :false
+
 let mainWindow;
 
 function createMainWindow()
@@ -8,10 +13,25 @@ function createMainWindow()
     title:"DunDung",
     width: 500,
     height: 600,
-    icon:`${__dirname}/asset/icon/info-panel.png`,
+    icon:`${__dirname}/asset/icon/A.png`,
+    resizable:isDev,
+    webPreferences: {
+      contextIsolation: true
+    }
   })
 
   mainWindow.loadFile('./app/index.html')
 }
 app.on('ready', createMainWindow)
+
+
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow()
+  }
+})
+
+
+app.allowRendererProcessReuse=true
   
